25bc51197aa53d1763e8dea2413c01b87609bfe0
franronci <franciscoronci@gmail.com>
2026-01-18 18:52:30 -0300

    add video_stats file

---
+++ video_stats.py
@@ -0,0 +1,31 @@
+import requests 
+import json 
+
+CHANNEL_HANDLE = 'SantiagoMagnin'
+
+def get_playlist_id():
+
+    try:
+
+        url=f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}'
+
+        response = requests.get(url)
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        channel_items = data["items"][0]
+
+        channel_playlist = channel_items["contentDetails"]["relatedPlaylists"]['uploads']
+
+        ##print(channel_playlist)
+
+        return channel_playlist
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+if __name__ == '__main__':
+
+    get_playlist_id()
