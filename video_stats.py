a5a74219e3606e2e687a2c80fc9ff31c63ad97ab
franronci <franciscoronci@gmail.com>
2026-01-19 10:56:13 -0300

    add something

---
+++ video_stats.py
@@ -0,0 +1,35 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env")
+
+CHANNEL_HANDLE = 'SantiagoMagnin'
+API_KEY = os.getenv("YOUTUBE_API_KEY") or os.getenv("API_KEY")
+
+def get_playlist_id():
+
+    try:
+
+        url=f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}'
+
+        response = requests.get(url)
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        channel_items = data["items"][0]
+
+        channel_playlist = channel_items["contentDetails"]["relatedPlaylists"]['uploads']
+
+        print(channel_playlist)
+
+        return channel_playlist
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+if __name__ == '__main__':
+
+    get_playlist_id()
